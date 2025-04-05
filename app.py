16a997375e3c424252a273b6bafba6611d9cfebe
hiranya25 <hiranyapatil20@gmail.com>
2025-04-05 16:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,6 +115,10 @@ def get_precomputed_tag_trends():
     except Exception as e:
         app.logger.error(f"Failed to return precomputed data: {str(e)}", exc_info=True)
         return jsonify({"error": "An internal error occurred while loading tag data."}), 500
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
+
